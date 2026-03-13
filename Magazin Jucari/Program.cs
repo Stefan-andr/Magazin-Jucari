127a3b932e8fbebfdd6dad09b373b8f1bbb6a836
Andronic Stefan <Andronic Stefan@DESKTOP-323Q1AO>
2026-03-13 21:03:32 +0200

    Add project files.

---
+++ Magazin Jucari/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Jucarie
+{
+    public string Nume { get; set; }
+    public double Pret { get; set; }
+    public string Categorie { get; set; }
+    public int Stoc { get; set; }
+
+    public Jucarie(string nume, double pret, string categorie, int stoc)
+    {
+     
+    }
+
+    public void Afiseaza()
+    {
+        Console.WriteLine("Nume: " + Nume);
+        Console.WriteLine("Pret: " + Pret + " lei");
+        Console.WriteLine("Categorie: " + Categorie);
+        Console.WriteLine("Stoc: " + Stoc);
+    }
+}
+
