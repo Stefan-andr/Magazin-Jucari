dc8be729aa7dc882d66dc57fd450b97f4440ab0f
Stefan-andr <stefan.andronic@student.usv.ro>
2026-04-02 14:24:02 +0300

    Delete Magazin Jucari directory

--- Magazin Jucari/IStocareData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-public interface IStocareData
-{
-    void AddJucarie(Jucarie j);
-    List<Jucarie> GetJucarii();
-    void SalveazaTot(List<Jucarie> lista);
-}
